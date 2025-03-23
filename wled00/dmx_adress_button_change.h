07c85855562db78ef7a29133138b75c5d8a8078b
Gideon Bühler <gideonbuehler18@gmail.com>
2025-03-23 22:58:23 +0100

    debug

--- wled00/dmx_adress_button_change.h
+++ wled00/dmx_adress_button_change.h
@@ -112,6 +112,9 @@ public:
     }
     if (curstate_btn_save == LOW && laststate_btn_save == HIGH) {
       btnSavePressed();
+      oled.setCursor(0, 30);
+      oled.setTextSize(1);
+      oled.println("saved");
     }
     if (curstate_btn_mode == LOW && laststate_btn_mode == HIGH) {
       btnModePressed();
